62f6d59ace386dd098253ba5565628dfecff7216
ohinkinen <inkinenolliheikki@gmail.com>
2024-08-25 11:18:06 +0300

    Add unit test for checkCSV middleware

---
+++ backend/src/tests/unit/checkCSV.test.ts
@@ -0,0 +1,9 @@
+/* Tests for the checkCSV middleware */
+// import checkCSV from "../../middleware/checkCSV";
+import { describe, test, expect } from "@jest/globals";
+
+describe("CheckCSV middleware", () => {
+    test('Should call next function if CSV file is included', () => {
+        
+    })
+})
